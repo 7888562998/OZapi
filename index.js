26222b7fcbd423d0f56ac6f69ac5a2026e710b2d
sarbjit <web2it@hotmail.co.uk>
2024-09-14 12:41:17 +0530

    index file changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Librarys
+// Libraries
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
@@ -10,34 +10,24 @@ import { fileURLToPath } from "url";
 import { connectDB } from "./src/DB/index.js";
 // Routes
 import { AuthRouters } from "./src/Router/AuthRouters.js";
-
-// Response Handler
 import { AdminRouters } from "./src/Router/AdminRouters.js";
-
 import { ResHandler } from "./src/Utils/ResponseHandler/ResHandler.js";
 import { ActivityRouters } from "./src/Router/ActivityRouters.js";
 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-
-export const filename = fileURLToPath(import.meta.url);
-export const dirname = path.dirname(filename);
-
-export let app = express();
-
-
-
+const app = express();
 const API_PreFix = "/api/v1";
-
 
 app.use("/", express.static("Uploads"));
 
-var corsOptions = {
+const corsOptions = {
   origin: "*",
 };
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
-// Configure body-parser to handle post requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
@@ -47,50 +37,31 @@ morganBody(app, {
   logReqUserAgent: true,
   logReqDateTime: true,
 });
-// Connect To Database
-// connectDB();
 
+// Connect To Database
+const start = async () => {
+  try {
+    await connectDB();
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the application." });
 });
+
 // Routes
-
-// Route For Auditors
 app.use(API_PreFix, AuthRouters);
-// ====// Bussiness Routes
-
-
-// Route For Admin
 app.use(API_PreFix, AdminRouters);
-
-// Route For Activity
 app.use(API_PreFix, ActivityRouters);
 
-
-
-// Hunt Routes
-
-const start = () => {
-  connectDB()
-    .then(() => {
-      console.log("MongoDB connected successfully");
-    })
-    .catch((err) => {
-      console.error("MongoDB connection error:", err);
-    });
-  app.get("/", async(req, res) => {
-    res.status(200).json("Api ready to use");
-  });
-  
+// Error Handling Middleware
 app.use(ResHandler);
-};
+
+// Default export
+export default app;
+
+// Call start function to initialize DB connection
 start();
-app.listen(3000, () => {
-  console.log(`connection is live to this port ${3000}`);
-});
-
-
-
-
-
